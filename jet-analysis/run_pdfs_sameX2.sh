8bd446a84269064de1301f9a0fbd9789d38b33af
pebaron <petr.barn@cern.ch>
2019-11-28 16:32:50 +0100

    pdf plots

--- jet-analysis/run_pdfs_sameX2.sh
+++ jet-analysis/run_pdfs_sameX2.sh
@@ -4,3 +4,5 @@ echo $1
 for i in CT14lo MMHT2014nlo68cl MRST2004qed MRST2004qed_proton CT14nlo MMHT2014lo68cl MRSTMCal MRST2004qed_neutron MRST2007lomod; do
 ./plot_pdfs_sameX2.py -n $i -q2 $1
 done;
+
+for i in $(cat listNNPDF.txt); do ./plot_pdfs_sameX2.py -n $i -q2 $1; done
